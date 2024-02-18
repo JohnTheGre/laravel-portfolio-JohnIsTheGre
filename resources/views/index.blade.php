028f736e855e22a05acafecadc9089586f8815be
osuntuyi john <osun0001@hz.nl>
2024-02-18 21:16:09 +0100

    things are appearing now

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,6 @@
-<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 {{--    <link href="styles.css" rel="stylesheet">--}}
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,10 +1,16 @@
-<nav class="navbar">
-    <ul class="content">
-        <li><a href="{{ route('/index') }}">Home Page</a></li>
-        <li><a href="{{ route('/profile') }}">Profile</a></li>
-        <li><a href="{{ route('/dashboard') }}">Dashboard</a></li>
-        <li><a href="{{ route('/faq') }}">FAQ</a></li>
-        <li><a href="{{ route('/blog') }}">Blog</a></li>
+{{--@include('index');--}}
+{{--@include('profile');--}}
+{{--@include('dashboard');--}}
+{{--@include('faq');--}}
+{{--@include('blog');--}}
+
+{{--<nav class="navbar">--}}
+{{--    <ul class="content">--}}
+{{--        <li><a href="{{ route('/index') }}">Home Page</a></li>--}}
+{{--        <li><a href="{{ route('/profile') }}">Profile</a></li>--}}
+{{--        <li><a href="{{ route('/dashboard') }}">Dashboard</a></li>--}}
+{{--        <li><a href="{{ route('/faq') }}">FAQ</a></li>--}}
+{{--        <li><a href="{{ route('/blog') }}">Blog</a></li>--}}
 {{--        <li><img src="{{ asset('Hz image-new.png') }}" alt="Hz university of applied sciences logo" id="logo"></li>--}}
-    </ul>
-</nav>
+{{--    </ul>--}}
+{{--</nav>--}}
